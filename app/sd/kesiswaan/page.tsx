35c084f3a9cede920a92aac5bf8787fc10d65090
Admin_Ganteng <115489699+bankyan80@users.noreply.github.com>
2026-06-28 13:56:04 +0700

    refactor: split kesiswaan into /sd/kesiswaan and /tk-kb/kesiswaan routes

---
+++ app/sd/kesiswaan/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import KesiswaanContent from '@/components/kesiswaan/KesiswaanContent'
+
+export default function SdKesiswaanPage() {
+  return <KesiswaanContent allowedJenjang={['sd']} defaultJenjang="sd" />
+}
---
+++ app/tk-kb/kesiswaan/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import KesiswaanContent from '@/components/kesiswaan/KesiswaanContent'
+
+export default function TkKbKesiswaanPage() {
+  return <KesiswaanContent allowedJenjang={['tk', 'kb']} defaultJenjang="tk" />
+}
